7a0661a936287d7f6960b100363d04643557ad4a
Jayshah026 <jayshah01.jds@gmail.com>
2026-04-14 16:02:53 +0530

    Enable user-defined pattern size input
    Modified the loop to prompt the user for the pattern size, allowing for dynamic input.

--- Patterns/P4.cpp
+++ Patterns/P4.cpp
@@ -10,7 +10,9 @@
 using namespace std;
 
 int main(){
-    for(int i = 1; i <= 5; i++){
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++){
         for(int j = 1; j <= i; j++){
             cout << i;
         }
