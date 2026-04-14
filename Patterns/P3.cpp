0ad33b98f3664921b607e5498b9b4a1bfc801209
Jayshah026 <jayshah01.jds@gmail.com>
2026-04-14 16:02:06 +0530

    Allow user to input number of rows for pattern
    Added user input for the number of rows in the pattern.

--- Patterns/P3.cpp
+++ Patterns/P3.cpp
@@ -11,7 +11,9 @@ To make this pattern :
 using namespace std;
 
 int main(){
-    for(int i = 1; i <= 5; i++){
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++){
         for(int j = 1; j <= i; j++){
             cout << j;
         }
