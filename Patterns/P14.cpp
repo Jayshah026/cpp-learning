d70cc192b46c7c430c8d370dd66ea3ad52eafdbb
Jayshah026 <jayshah01.jds@gmail.com>
2026-04-19 04:15:56 +0530

    Add pattern : alphabet triangle
    Implement a program to print a pattern of letters from A to the nth letter.

---
+++ Patterns/P14.cpp
@@ -0,0 +1,24 @@
+/*To print this pattern : 
+A
+AB
+ABC
+ABCD
+ABCDE         */
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++){
+        for(char j = 'A'; j <= 'A' + i ; j++){
+                cout << j;
+        }
+            cout << endl;
+   }
+        return 0;
+}
+
+// Here if i have written "j <= 'A' + i" Then the output will be starting form AB not form the A. 
