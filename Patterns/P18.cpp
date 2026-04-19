f08fa3322142f7c0d2384c0b0a98dc135370fa86
Jayshah026 <jayshah01.jds@gmail.com>
2026-04-19 15:28:57 +0530

    Add pattern : reverse alphabet triangle

---
+++ Patterns/P18.cpp
@@ -0,0 +1,25 @@
+/*To print this pattern : 
+
+E
+DE
+CDE
+BCDE
+ABCDE
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        for(char j = 'E' - i; j <= 'E'; j++){
+            cout << j;
+        }
+        cout << endl;
+    }
+    return 0;
+}
