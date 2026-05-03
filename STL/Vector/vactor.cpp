e193070ecbd0834b0a1eb1cc54eb85071ad9481b
Jayshah026 <jayshah01.jds@gmail.com>
2026-05-03 15:29:38 +0530

    Fix vector insertion and value assignment
    Updated vector insertion logic and modified value assignments.

--- STL/Vector/vactor.cpp
+++ STL/Vector/vactor.cpp
@@ -5,6 +5,7 @@ Here What we will learn..
 - How to replace values.
 - How to insert values using "push_back" and "emplace_back". 
 - How to show the values of the vector using for loop. 
+- Inserting an element at the fornt.
 */
 
 
@@ -16,8 +17,9 @@ int main(){
     vector<int> v(5, 100);
     v.push_back(54);
     v.emplace_back(432);
+    v.insert(v.begin(), 302);
 
-    v[0] = 34;
+    v[2] = 34;
     v[1] = 335;
 
     for(int num : v){
@@ -30,8 +32,9 @@ int main(){
 
 OUTPUT : 
 
-34 
+302
 335
+34
 100
 100
 100
