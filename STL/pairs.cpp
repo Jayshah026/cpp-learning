8d151a6af5502272239b1d492e10093c411d8963
Jayshah026 <jayshah01.jds@gmail.com>
2026-05-03 14:26:26 +0530

    Add example of using pairs in C++
    Implement a simple program to demonstrate the use of pairs in C++.

---
+++ STL/pairs.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    pair<int, int> p ={1, 4};
+
+    cout << p.first << " " << p.second;
+    return 0;
+}
+
+// Here the output will be 1 and 4 
