1f26b289b8b2209d676054ce4890c4d490ea8540
Jayshah026 <jayshah01.jds@gmail.com>
2026-05-11 15:31:08 +0530

    Add Fibonacci recursive function in C++
    Implement a recursive function to calculate Fibonacci numbers.

---
+++ Recursion_Basic/Fibonacci.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int fibonacciNum(int n){
+    if(n <= 1){
+        return n;
+    }else{
+        int last = fibonacciNum(n - 1);
+        int second_last = fibonacciNum(n - 2);
+        return last + second_last;
+    }
+}
+
+int main(){
+    cout << fibonacciNum(9);
+    return 0;
+}
+
+
+OUTPUT : 
+
+34
+// Here if you want another output then you can change the number in main function which is right now is 9 but if you change it, the output will be changed too. 
