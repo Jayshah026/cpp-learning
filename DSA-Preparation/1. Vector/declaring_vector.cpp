82ef07c907775265cd38e615381ad5d9157a7aaf
Jayshah026 <jayshah01.jds@gmail.com>
2026-07-31 13:13:12 +0530

    Declaring the vector

---
+++ DSA-Preparation/1. Vector/declaring_vector.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> v1;
+    vector<float> v2(10);
+    vector<int> v3(5, 100);
+    vector<int> v4 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+
+    cout << "accessing the v4" << v4.size();
+}
